a834ac8c2feeebc2df37ace9d8b48143799760c5
rfzu <rall@list.ru>
2014-10-21 17:43:11 +0400

    before 9.3.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,10 +27,11 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Users',        href: users_path) }
+      it { should have_link('Profile',      href: user_path(user)) }
       it { should have_link('Settings',     href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Sign out',     href: signout_path) }
+      it { should_not have_link('Sign in',  href: signin_path) }
 
       describe "folowed by sighout" do
         before { click_link "Sign out" }
@@ -92,6 +93,11 @@ describe "Authentication" do
             before { patch user_path(wrong_user) }
             specify { expect(response).to redirect_to(root_url) }
           end
+        end
+
+        describe "visiting the user index" do 
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
 
       end
