afd61a669e7a9366f32418fef7c9db776afcb78a
rfzu <rall@list.ru>
2014-10-21 18:52:45 +0400

    before 9.3

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name                  "Hartl"
-    email                 "hartl@list.ru"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password              "foobar"
     password_confirmation "foobar"
   end  
