ddef2d19b48d0f8af877ce3080ea45c08ae22e82
rfzu <rall@list.ru>
2014-10-24 16:05:51 +0400

    end Before ajax

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -41,7 +41,7 @@ describe "Authentication" do
 
   end
 
-  describe "autorization" do
+  describe "authorization" do
 
   describe "for non-signed-in users" do
     let(:user) { FactoryGirl.create(:user) }
@@ -66,6 +66,11 @@ describe "Authentication" do
         describe "visiting the edit page" do 
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }          
+        end
+
+        describe "visiting the followers page" do 
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
         end
 
         describe "submitting to the update action" do
@@ -128,5 +133,17 @@ describe "Authentication" do
       end
     end
 
+    describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
+    
   end
 end
